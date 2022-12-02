86fe9ffbf555c30422616900e3443b62c26bf65a
Digitalu <87531803+digitalu@users.noreply.github.com>
2022-12-02 15:04:07 +0800

    Update express.ts

--- package/server/adapters/express.ts
+++ package/server/adapters/express.ts
@@ -1,10 +1,11 @@
 import { BridgeRoutes } from '../../routes';
 import { ErrorHandler } from '../../error';
 import type * as express from 'express';
+import formidableLib from 'formidable';
 
 import { createHttpHandler } from './node-http';
 
 export const createExpressMiddleware = (
   routes: BridgeRoutes,
-  { errorHandler }: { errorHandler?: ErrorHandler },
-): express.Handler => createHttpHandler(routes, { errorHandler });
+  { errorHandler, formidable }: { errorHandler?: ErrorHandler; formidable?: typeof formidableLib },
+): express.Handler => createHttpHandler(routes, { errorHandler, formidable });
